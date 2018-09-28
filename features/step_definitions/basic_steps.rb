1a800c844e5b79307e6e2725dac8cedf9b115f85
Miriam_Sallnäs <miriamfly54@hotmail.com>
2018-09-28 11:48:22 +0200

    Feature test product

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -5,3 +5,19 @@ Given("I visit the landing page") do
   Then("I should see {string}") do |content|
     expect(page).to have_content content
   end
+
+  Given("the following products exists") do |table|
+    table.hashes.each do |product_name| 
+      create(:product, product_name)
+    end
+  end
+  
+  Given("I visit the product page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should see {string} for {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  
